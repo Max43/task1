0d19ead42223303fe22e06cf8dfee444362e2feb
Max43 <m.stukanov43@gmail.com>
2015-07-02 02:46:46 +0300

    change config/create

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,5 +1,6 @@
-get '/orders', to: 'orders#create'
-get '/orders', to: 'orders#new'
+#get '/orders', to: 'orders#create'
+#get '/orders', to: 'orders#new'
 get '/home', to: 'home#index'
+resources 'orders', only: [:new, :create]
 # Configure your routes here
 # See: http://www.rubydoc.info/gems/lotus-router/#Usage
--- apps/web/controllers/orders/create.rb
+++ apps/web/controllers/orders/create.rb
@@ -2,7 +2,22 @@ module Web::Controllers::Orders
   class Create
     include Web::Action
 
+     params do
+	     param :name 
+	     param :quotation 
+	     param :company_name
+	     param :email
+	     param :url
+	     param :address   
+	     param :city    
+	     param :state     
+	     param :zip      
+	     param :phone_number
+    end
+
+
     def call(params)
+    	@order = OrderRepository.create(Order.new(params))
     end
   end
 end
